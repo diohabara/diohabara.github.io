4ada38e817ca9f1dd8cb15ccac938f21c616b4ef
diohabara <diohabara@gmail.com>
2021-02-27 21:16:53 +0900

    Deploy to production

---
+++ blog/config.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  url: 'https://diohabara.github.io',
+  pathPrefix: '/',
+  title: 'Potsu Potsu',
+  subtitle: '日本語で書きます',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-134331234-2',
+  useKatex: true,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+  ],
+  author: {
+    name: 'diohabara',
+    photo: '/photo.png',
+    bio: ' A mouse ',
+    contacts: {
+      email: '',
+      facebook: '',
+      telegram: '',
+      twitter: '',
+      github: 'diohabara',
+      rss: '',
+      vkontakte: '',
+      linkedin: '',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: '',
+    }
+  }
+};
